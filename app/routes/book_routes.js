4d4e497c9a25b2fb69e6764fe42b8012166c1b87
Daniel Mbeyah <danmbeyah@gmail.com>
2018-01-27 20:51:19 +0300

    Initial commit

---
+++ app/routes/book_routes.js
@@ -0,0 +1,58 @@
+var ObjectID = require('mongodb').ObjectID
+
+module.exports = function(app,db){
+	//create book
+	app.post('/books', (req, res) =>{
+		//create book
+		//console.log(req.body);
+		//res.send('Hey John Snow, welcome to Kings landing');
+		const book = { summary: req.body.summary, title: req.body.title , author: req.body.author};
+		db.collection('books').insert(book, (err, result) => {
+			if(err){
+				res.send({'error': 'An error has ocurred'})
+			}else{
+				res.send(result.ops[0])
+			}
+		})
+	})
+
+	//get book by id
+	app.get('/books/:id', (req, res) => {
+		const id = req.params.id;
+		const details = {'_id': new ObjectID(id)}; //mongo requires id as an object, not just a simple string
+		db.collection('books').findOne(details, (err, item) =>{
+			if(err){
+				res.send({'error': 'An error has ocurred'})
+			}else{
+				res.send(item)
+			}
+		})
+	})
+
+	//delete book by id
+	app.delete('/books/:id', (req, res) => {
+		const id = req.params.id;
+		const details = {'_id': new ObjectID(id)};
+		db.collection('books').remove(details, (err, item) =>{
+			if(err){
+				res.send({'error': 'An error has ocurred'})
+			}else{
+				res.send('Book ' + id + ' deleted successfully ')
+			}
+		})
+	})
+
+	//update book by id
+	app.put('/books/:id', (req, res) => {
+		const id = req.params.id;
+		const details = {'_id': new ObjectID(id)};
+		const book = { text: req.body.book_body, title: req.body.title };
+		db.collection('books').update(details, book, (err, item) =>{
+			if(err){
+				res.send({'error': 'An error has ocurred'})
+			}else{
+				res.send('Book ' + id + ' updated successfully ')
+			}
+		})
+	})
+}
---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+	url: "mongodb://root:password@ds117128.mlab.com:17128/books"
+}
