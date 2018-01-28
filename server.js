670dd97703ccd8b053eeb4cdc3b9bc75a130948e
Daniel <danmbeyah@gmail.com>
2018-01-28 12:21:15 +0300

    Port change

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const db = require('./config/db');
  
 const app = express();
-const port = 8000;
+const port = 38260;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
